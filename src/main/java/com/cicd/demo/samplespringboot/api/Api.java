447ba39745e07085ffae216f0b149f7ba5d3af75
Iftekhar Shaikh <aiftekharshaikh@deloitte.com>
2021-08-21 13:58:11 +0530

    third commit

--- src/main/java/com/cicd/demo/samplespringboot/api/Api.java
+++ src/main/java/com/cicd/demo/samplespringboot/api/Api.java
@@ -11,7 +11,7 @@ public class Api {
 
     @GetMapping("/message")
     public String getMessage(){
-        return "Hello World - 2!! I am deployed";
+        return "Hello World - 2!! I am deployed on Azure";
     }
 
 }
