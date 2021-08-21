32d375c0caf5edfa4887b25abd2444a5c68fc64c
Iftekhar Shaikh <aiftekharshaikh@deloitte.com>
2021-08-21 13:30:34 +0530

    first commit

---
+++ src/main/java/com/cicd/demo/samplespringboot/api/Api.java
@@ -0,0 +1,17 @@
+package com.cicd.demo.samplespringboot.api;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Api {
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return "Hello!!";
+    }
+
+}
---
+++ src/test/java/com/cicd/demo/samplespringboot/SampleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.demo.samplespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
